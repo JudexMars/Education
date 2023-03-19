08eb73541167d95b0d173d2579a11dcf055d84ce
JudexMars <danielpro2013rus@gmail.com>
2023-03-19 21:49:43 +0300

    Tinkoff Contest Task 9

--- Tinkoff/Task9/Main.java
+++ Tinkoff/Task9/Main.java
@@ -11,32 +11,31 @@ public class Main {
             List<Integer> prices = new ArrayList<>();
             for (int i = 0; i < n; i++) prices.add(in.nextInt());
 
-            int[][] table = new int[n + 1][n + 1];
-            
-            //fill(table, Integer.MAX_VALUE);
+            int[][] table = new int[n  + 1][n + 1];
+             
+
+            fill(table, 100000);
+
             table[0][0] = 0;
 
-            for (int i = 1; i < n + 1; i++) {
-                table[0][i] = Integer.MAX_VALUE;
-                //if (i != 0) table[0][i] += table[0][i - 1];
-                table[i][0] = prices.get(i - 1);
-                //table[i][0] = 0;
-            }
+            // for (int i = 0; i < n; i++) {
+            //     table[0][i] = Integer.MAX_VALUE;
+            // }
 
-            for (int i = 1; i < n; i++) {
-                for (int j = 1; j < n; j++) {
-                    //if (i == 0 && j == 0) continue;
-                    if (prices.get(i - 1) <= 100) {
-                        System.out.println(table[i - 1][j]);
-                        table[i][j] = Math.min(table[i][j - 1] + prices.get(i - 1), table[i - 1][j + 1]);
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    table[i][j] = Math.min(table[i][j], table[i - 1][j] + prices.get(i - 1));
+                    if (prices.get(i - 1) > 100) {
+                        table[i][j + 1] = Math.min(table[i][j + 1], table[i - 1][j] + prices.get(i - 1));
                     }
-                        else {
-                        table[i][j] = Math.min(table[i - 1][j - 1] + prices.get(i - 1), table[i - 1][j + 1]);
+                    if (j >= 1) {
+                        table[i][j - 1] = Math.min(table[i][j - 1], table[i - 1][j]);
                     }
+
                 }
             }
-            print2DArray(table);
-            System.out.println(findMinInColumn(table, n - 1));
+            //print2DArray(table);
+            System.out.println(findMinInColumn(table, n));
         }
     }
 
